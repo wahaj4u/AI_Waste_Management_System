82eb4739ed302fb0e97a61974764ecb376799f2e
Wahaj Ul Islam <33065153+wahaj4u@users.noreply.github.com>
2024-12-19 19:15:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from PIL import Image
 import numpy as np
@@ -291,4 +292,7 @@ def process_image():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
+    #app.run(debug=True)
