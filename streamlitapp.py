7b6541bebd97c98761c1f2207e6b51173f0fbe7b
Wahaj Ul Islam <33065153+wahaj4u@users.noreply.github.com>
2024-12-29 21:33:53 +0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,9 +1,12 @@
 import torch
 from segment_anything import sam_model_registry
 
-checkpoint_path = 'sam_vit_b.pth'
+# Set up the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-sam = sam_model_registry['vit_b'](checkpoint=checkpoint_path)
+# Load the model checkpoint with weights only
+checkpoint_path = 'sam_vit_b.pth'
+sam = sam_model_registry['vit_b'](checkpoint=checkpoint_path, weights_only=True)
 sam.to(device)
+
 print("Model loaded successfully!")
