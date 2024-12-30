0454cbe0de87da77de009083dece0a195f145f93
Wahaj Ul Islam <33065153+wahaj4u@users.noreply.github.com>
2024-12-30 16:25:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def classify_waste():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if $PORT is not set
+    app.run(host='0.0.0.0', port=port)
