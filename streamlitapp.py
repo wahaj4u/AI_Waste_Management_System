26f54f0a5aa61c2e0b40ad4f45b910af1b1cd19f
Wahaj Ul Islam <33065153+wahaj4u@users.noreply.github.com>
2025-01-04 15:20:21 +0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -43,34 +43,7 @@ disposal_methods = {
 }
 
 # Initialize SAM model
-def load_sam_model():
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_path = 'sam_vit_b.pth'
-    
-    # Google Drive file ID from the link
-    google_drive_file_id = '1_KcHtbr2x7wxCHRLKn1r5jSUY_wK3POe'  # Extracted file ID from your link
-    google_drive_url = f'https://drive.google.com/uc?id={google_drive_file_id}'
 
-    # Check if the model is already downloaded
-    if not os.path.exists(model_path):
-        print(f"Downloading model from Google Drive: {google_drive_url}")
-        
-        # Download the model from Google Drive
-        gdown.download(google_drive_url, model_path, quiet=False)
-
-    if os.path.exists(model_path):
-        print(f"Loading model from: {model_path}")
-        
-        # Load the model state_dict
-        state_dict = torch.load(model_path, map_location=torch.device('cpu'))
-        
-        # Now load the model with the state_dict
-        sam = sam_model_registry['vit_b'](checkpoint=state_dict)
-        
-        sam.to(device)
-        return SamAutomaticMaskGenerator(sam)
-    else:
-        st.error("Model file could not be downloaded!")
 
 # Load trained classification model
 @st.cache_resource
